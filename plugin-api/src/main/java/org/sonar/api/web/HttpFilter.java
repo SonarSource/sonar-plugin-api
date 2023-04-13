32c5438d17ef62062baf41ba11610809d18766b8
Jacek Poreda <jacek.poreda@sonarsource.com>
2023-04-13 18:06:44 +0200

    PLUGINAPI-42 Introduce HttpFilter class and deprecate ServletFilter which is using javax.servlet.*

---
+++ plugin-api/src/main/java/org/sonar/api/web/HttpFilter.java
@@ -0,0 +1,101 @@
+/*
+ * Sonar Plugin API
+ * Copyright (C) 2009-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.api.web;
+
+import java.io.IOException;
+
+import org.sonar.api.ExtensionPoint;
+import org.sonar.api.server.ServerSide;
+import org.sonar.api.server.http.HttpRequest;
+import org.sonar.api.server.http.HttpResponse;
+
+/**
+ * A filter is an object that performs filtering tasks on either the
+ * request to a resource (a servlet or static content), or on the response
+ * from a resource, or both..
+ *
+ * @since 9.16
+ */
+@ServerSide
+@ExtensionPoint
+public abstract class HttpFilter {
+
+  /**
+   * This method is called exactly once after instantiating the filter. The init
+   * method must complete successfully before the filter is asked to do any
+   * filtering work.
+   */
+  public abstract void init();
+
+
+  /**
+   * The <code>doFilter</code> method of the Filter is called by the
+   * SonarQube each time a request/response pair is passed through the
+   * chain due to a client request for a resource at the end of the chain.
+   * The FilterChain passed in to this method allows the Filter to pass
+   * on the request and response to the next entity in the chain.
+   *
+   * <p>A typical implementation of this method would follow the following
+   * pattern:
+   * <ol>
+   * <li>Examine the request
+   * <li>Optionally wrap the request object with a custom implementation to
+   * filter content or headers for input filtering
+   * <li>Optionally wrap the response object with a custom implementation to
+   * filter content or headers for output filtering
+   * <li>
+   * <ul>
+   * <li><strong>Either</strong> invoke the next entity in the chain
+   * using the FilterChain object
+   * (<code>chain.doFilter()</code>),
+   * <li><strong>or</strong> not pass on the request/response pair to
+   * the next entity in the filter chain to
+   * block the request processing
+   * </ul>
+   * <li>Directly set headers on the response after invocation of the
+   * next entity in the filter chain.
+   * </ol>
+   */
+  public abstract void doFilter(HttpRequest request, HttpResponse response, FilterChain chain) throws IOException;
+
+
+  /**
+   * Called by the SonarQube to indicate to a filter that it is being
+   * taken out of service.
+   *
+   * <p>This method is only called once all threads within the filter's
+   * doFilter method have exited or after a timeout period has passed.
+   * After the web container calls this method, it will not call the
+   * doFilter method again on this instance of the filter.
+   *
+   * <p>This method gives the filter an opportunity to clean up any
+   * resources that are being held (for example, memory, file handles,
+   * threads) and make sure that any persistent state is synchronized
+   * with the filter's current state in memory.
+   */
+  public abstract void destroy();
+
+  /**
+   * Override to change URL. Default is /*
+   */
+  public UrlPattern doGetPattern() {
+    return UrlPattern.builder().build();
+  }
+}
---
+++ plugin-api/src/test/java/org/sonar/api/web/UrlPatternTest.java
@@ -0,0 +1,230 @@
+/*
+ * Sonar Plugin API
+ * Copyright (C) 2009-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.api.web;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class UrlPatternTest {
+
+  @Test
+  public void include_all() {
+    UrlPattern pattern = UrlPattern.create("/*");
+    assertThat(pattern.matches("/")).isTrue();
+    assertThat(pattern.matches("/foo/ooo")).isTrue();
+
+    assertThat(pattern.getInclusions()).containsOnly("/*");
+    assertThat(pattern.getExclusions()).isEmpty();
+  }
+
+  @Test
+  public void include_end_of_url() {
+    UrlPattern pattern = UrlPattern.create("*foo");
+    assertThat(pattern.matches("/")).isFalse();
+    assertThat(pattern.matches("/hello/foo")).isTrue();
+    assertThat(pattern.matches("/hello/bar")).isFalse();
+    assertThat(pattern.matches("/foo")).isTrue();
+    assertThat(pattern.matches("/foo2")).isFalse();
+  }
+
+  @Test
+  public void include_beginning_of_url() {
+    UrlPattern pattern = UrlPattern.create("/foo/*");
+    assertThat(pattern.matches("/")).isFalse();
+    assertThat(pattern.matches("/foo")).isTrue();
+    assertThat(pattern.matches("/foo/bar")).isTrue();
+    assertThat(pattern.matches("/bar")).isFalse();
+  }
+
+  @Test
+  public void include_exact_url() {
+    UrlPattern pattern = UrlPattern.create("/foo");
+    assertThat(pattern.matches("/")).isFalse();
+    assertThat(pattern.matches("/foo")).isTrue();
+    assertThat(pattern.matches("/foo/")).isFalse();
+    assertThat(pattern.matches("/bar")).isFalse();
+  }
+
+  @Test
+  public void exclude_all() {
+    UrlPattern pattern = UrlPattern.builder()
+      .excludes("/*")
+      .build();
+    assertThat(pattern.matches("/")).isFalse();
+    assertThat(pattern.matches("/foo/ooo")).isFalse();
+  }
+
+  @Test
+  public void exclude_end_of_url() {
+    UrlPattern pattern = UrlPattern.builder()
+      .excludes("*foo")
+      .build();
+
+    assertThat(pattern.matches("/")).isTrue();
+    assertThat(pattern.matches("/hello/foo")).isFalse();
+    assertThat(pattern.matches("/hello/bar")).isTrue();
+    assertThat(pattern.matches("/foo")).isFalse();
+    assertThat(pattern.matches("/foo2")).isTrue();
+  }
+
+  @Test
+  public void exclude_beginning_of_url() {
+    UrlPattern pattern = UrlPattern.builder()
+      .excludes("/foo/*")
+      .build();
+
+    assertThat(pattern.matches("/")).isTrue();
+    assertThat(pattern.matches("/foo")).isFalse();
+    assertThat(pattern.matches("/foo/bar")).isFalse();
+    assertThat(pattern.matches("/bar")).isTrue();
+  }
+
+  @Test
+  public void exclude_exact_url() {
+    UrlPattern pattern = UrlPattern.builder()
+      .excludes("/foo")
+      .build();
+
+    assertThat(pattern.matches("/")).isTrue();
+    assertThat(pattern.matches("/foo")).isFalse();
+    assertThat(pattern.matches("/foo/")).isTrue();
+    assertThat(pattern.matches("/bar")).isTrue();
+  }
+
+  @Test
+  public void use_multiple_include_patterns() {
+    UrlPattern pattern = UrlPattern.builder()
+      .includes("/foo", "/foo2")
+      .build();
+    assertThat(pattern.matches("/")).isFalse();
+    assertThat(pattern.matches("/foo")).isTrue();
+    assertThat(pattern.matches("/foo2")).isTrue();
+    assertThat(pattern.matches("/foo/")).isFalse();
+    assertThat(pattern.matches("/bar")).isFalse();
+  }
+
+  @Test
+  public void use_multiple_exclude_patterns() {
+    UrlPattern pattern = UrlPattern.builder()
+      .excludes("/foo", "/foo2")
+      .build();
+    assertThat(pattern.matches("/")).isTrue();
+    assertThat(pattern.matches("/foo")).isFalse();
+    assertThat(pattern.matches("/foo2")).isFalse();
+    assertThat(pattern.matches("/foo/")).isTrue();
+    assertThat(pattern.matches("/bar")).isTrue();
+  }
+
+  @Test
+  public void use_include_and_exclude_patterns() {
+    UrlPattern pattern = UrlPattern.builder()
+      .includes("/foo/*", "/foo/lo*")
+      .excludes("/foo/login", "/foo/logout", "/foo/list")
+      .build();
+    assertThat(pattern.matches("/")).isFalse();
+    assertThat(pattern.matches("/foo")).isTrue();
+    assertThat(pattern.matches("/foo/login")).isFalse();
+    assertThat(pattern.matches("/foo/logout")).isFalse();
+    assertThat(pattern.matches("/foo/list")).isFalse();
+    assertThat(pattern.matches("/foo/locale")).isTrue();
+    assertThat(pattern.matches("/foo/index")).isTrue();
+  }
+
+  @Test
+  public void use_include_and_exclude_prefix() {
+    UrlPattern pattern = UrlPattern.builder()
+      .includes("/foo_2")
+      .excludes("/foo")
+      .build();
+    assertThat(pattern.matches("/")).isFalse();
+    assertThat(pattern.matches("/foo_2")).isTrue();
+    assertThat(pattern.matches("/foo")).isFalse();
+  }
+
+  @Test
+  public void exclude_pattern_has_higher_priority_than_include_pattern() {
+    UrlPattern pattern = UrlPattern.builder()
+      .includes("/foo")
+      .excludes("/foo")
+      .build();
+    assertThat(pattern.matches("/foo")).isFalse();
+  }
+
+  @Test
+  public void accept_empty_patterns() {
+    UrlPattern pattern = UrlPattern.builder()
+      .excludes()
+      .includes()
+      .build();
+    assertThat(pattern.matches("/")).isTrue();
+    assertThat(pattern.matches("/foo/bar")).isTrue();
+  }
+
+  @Test
+  public void create_throws_IAE_if_empty_url() {
+    assertThatThrownBy(() -> UrlPattern.create(""))
+      .isInstanceOf(IllegalArgumentException.class)
+      .hasMessage("URL pattern must start with slash '/': ");
+  }
+
+  @Test
+  public void getUrl_returns_single_inclusion() {
+    assertThat(UrlPattern.create("/*").getInclusions()).containsOnly("/*");
+    assertThat(UrlPattern.create("/foo/bar").getInclusions()).containsOnly("/foo/bar");
+  }
+
+  @Test
+  public void test_staticResourcePatterns() {
+    assertThat(UrlPattern.Builder.staticResourcePatterns()).containsOnly(
+      "*.css",
+      "*.css.map",
+      "*.ico",
+      "*.png",
+      "*.jpg",
+      "*.jpeg",
+      "*.gif",
+      "*.svg",
+      "*.js",
+      "*.js.map",
+      "*.pdf",
+      "*.woff2",
+      "/json/*",
+      "/static/*",
+      "/robots.txt",
+      "/favicon.ico",
+      "/apple-touch-icon*",
+      "/mstile*");
+  }
+
+  @Test
+  public void test_label() {
+    assertThat(UrlPattern.builder().build().label()).isEqualTo("UrlPattern{inclusions=[], exclusions=[]}");
+    assertThat(UrlPattern.builder()
+      .includes("/foo/*")
+      .excludes("/foo/login")
+      .build().label()).isEqualTo("UrlPattern{inclusions=[/foo/*], exclusions=[/foo/login]}");
+    assertThat(UrlPattern.builder()
+      .includes("/foo/*", "/foo/lo*")
+      .excludes("/foo/login", "/foo/logout", "/foo/list")
+      .build().label()).isEqualTo("UrlPattern{inclusions=[/foo/*, ...], exclusions=[/foo/login, ...]}");
+  }
+}
