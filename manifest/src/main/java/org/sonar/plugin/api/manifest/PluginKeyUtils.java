7136815fa4255c9f58af5e6f40349b5b47c7ed64
Julien HENRY <julien.henry@sonarsource.com>
2026-02-06 11:52:14 +0100

    PLUGINAPI-178 Move the plugin manifest tooling to the sonar-plugin-api repo
    This make sense because the Manifest is part of the plugin contract, even if is not directly part of the Java API. Many components are duplicating this code (Scanners, SonarLint, SQS/SQC, Update Center, Maven Packaging plugin...)

---
+++ manifest/src/main/java/org/sonar/plugin/api/manifest/PluginKeyUtils.java
@@ -0,0 +1,62 @@
+/*
+ * Sonar Plugin API
+ * Copyright (C) 2009-2025 SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.plugin.api.manifest;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nullable;
+import org.apache.commons.lang3.Strings;
+
+public final class PluginKeyUtils {
+
+  private static final String SONAR_PLUGIN_SUFFIX = "-sonar-plugin";
+  private static final String SONAR_PREFIX = "sonar-";
+  private static final String PLUGIN_SUFFIX = "-plugin";
+
+  private PluginKeyUtils() {
+    // only static methods
+  }
+
+  @CheckForNull
+  public static String sanitize(@Nullable String mavenArtifactId) {
+    if (mavenArtifactId == null) {
+      return null;
+    }
+
+    String key = mavenArtifactId;
+    if (Strings.CS.startsWith(mavenArtifactId, SONAR_PREFIX) && Strings.CS.endsWith(mavenArtifactId, PLUGIN_SUFFIX)) {
+      key = Strings.CS.removeEnd(Strings.CS.removeStart(mavenArtifactId, SONAR_PREFIX), PLUGIN_SUFFIX);
+    } else if (Strings.CS.endsWith(mavenArtifactId, SONAR_PLUGIN_SUFFIX)) {
+      key = Strings.CS.removeEnd(mavenArtifactId, SONAR_PLUGIN_SUFFIX);
+    }
+    return keepLettersAndDigits(key);
+  }
+
+  private static String keepLettersAndDigits(String key) {
+    StringBuilder sb = new StringBuilder();
+    for (int index = 0; index < key.length(); index++) {
+      char character = key.charAt(index);
+      if (Character.isLetter(character) || Character.isDigit(character)) {
+        sb.append(character);
+      }
+    }
+    return sb.toString();
+  }
+
+}
---
+++ manifest/src/main/java/org/sonar/plugin/api/manifest/RequiredPlugin.java
@@ -0,0 +1,53 @@
+/*
+ * Sonar Plugin API
+ * Copyright (C) 2009-2025 SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.plugin.api.manifest;
+
+import java.util.regex.Pattern;
+import org.apache.commons.lang3.StringUtils;
+
+public class RequiredPlugin {
+
+  private static final Pattern PARSER = Pattern.compile("\\w+:.+");
+
+  private final String key;
+  private final Version minimalVersion;
+
+  public RequiredPlugin(String key, Version minimalVersion) {
+    this.key = key;
+    this.minimalVersion = minimalVersion;
+  }
+
+  public String getKey() {
+    return key;
+  }
+
+  public Version getMinimalVersion() {
+    return minimalVersion;
+  }
+
+  static RequiredPlugin parse(String s) {
+    if (!PARSER.matcher(s).matches()) {
+      throw new IllegalArgumentException("Manifest field does not have correct format: " + s);
+    }
+    var fields = StringUtils.split(s, ':');
+    return new RequiredPlugin(fields[0], Version.create(fields[1]).removeQualifier());
+  }
+
+}
