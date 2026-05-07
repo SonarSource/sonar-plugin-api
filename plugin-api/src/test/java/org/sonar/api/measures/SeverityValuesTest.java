4c50a85c320613443910bead381d68be92dee87b
alexander.orlov <alexander.orlov@sonarsource.com>
2026-05-07 17:39:30 +0200

    PLUGINAPI-189 Add test coverage

---
+++ plugin-api/src/test/java/org/sonar/api/measures/SeverityValuesTest.java
@@ -0,0 +1,59 @@
+/*
+ * Sonar Plugin API
+ * Copyright (C) SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.api.measures;
+
+import org.junit.jupiter.api.Test;
+import org.sonar.api.issue.impact.Severity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.sonar.api.rule.Severity.BLOCKER;
+import static org.sonar.api.rule.Severity.CRITICAL;
+import static org.sonar.api.rule.Severity.INFO;
+import static org.sonar.api.rule.Severity.MAJOR;
+import static org.sonar.api.rule.Severity.MINOR;
+
+class SeverityValuesTest {
+
+  @Test
+  void fromRuleSeverity_mapsAllValues() {
+    assertThat(SeverityValues.fromRuleSeverity(INFO)).isEqualTo(SeverityValues.INFO);
+    assertThat(SeverityValues.fromRuleSeverity(MINOR)).isEqualTo(SeverityValues.LOW);
+    assertThat(SeverityValues.fromRuleSeverity(MAJOR)).isEqualTo(SeverityValues.MEDIUM);
+    assertThat(SeverityValues.fromRuleSeverity(CRITICAL)).isEqualTo(SeverityValues.HIGH);
+    assertThat(SeverityValues.fromRuleSeverity(BLOCKER)).isEqualTo(SeverityValues.BLOCKER);
+  }
+
+  @Test
+  void fromRuleSeverity_throwsOnUnknownValue() {
+    assertThatIllegalArgumentException()
+      .isThrownBy(() -> SeverityValues.fromRuleSeverity("UNKNOWN"))
+      .withMessageContaining("UNKNOWN");
+  }
+
+  @Test
+  void fromImpactSeverity_mapsAllValues() {
+    assertThat(SeverityValues.fromImpactSeverity(Severity.INFO)).isEqualTo(SeverityValues.INFO);
+    assertThat(SeverityValues.fromImpactSeverity(Severity.LOW)).isEqualTo(SeverityValues.LOW);
+    assertThat(SeverityValues.fromImpactSeverity(Severity.MEDIUM)).isEqualTo(SeverityValues.MEDIUM);
+    assertThat(SeverityValues.fromImpactSeverity(Severity.HIGH)).isEqualTo(SeverityValues.HIGH);
+    assertThat(SeverityValues.fromImpactSeverity(Severity.BLOCKER)).isEqualTo(SeverityValues.BLOCKER);
+  }
+}
